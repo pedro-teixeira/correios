28da30486a12cce99591cde637642a5fede1455c
jarjar123 <rafaelpatro@gmail.com>
2014-12-17 18:10:03 -0200

    #17 Adicionado serviço de Carta Comercial

--- app/code/community/PedroTeixeira/Correios/Model/Source/PostMethods.php
+++ app/code/community/PedroTeixeira/Correios/Model/Source/PostMethods.php
@@ -30,6 +30,8 @@ class PedroTeixeira_Correios_Model_Source_PostMethods
             array('value' => 40290, 'label' => Mage::helper('adminhtml')->__('Sedex HOJE (40290)')),
             array('value' => 40045, 'label' => Mage::helper('adminhtml')->__('Sedex a Cobrar (40045)')),
             array('value' => 41300, 'label' => Mage::helper('adminhtml')->__('PAC GF (41300)')),
+            array('value' => 10065, 'label' => Mage::helper('adminhtml')->__('Carta Comercial (10065)')),
+            array('value' => 10138, 'label' => Mage::helper('adminhtml')->__('Carta Comercial Registrada (10138)')),
         );
     }
 }
