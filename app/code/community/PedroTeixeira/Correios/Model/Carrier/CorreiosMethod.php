a3ee9e936fcac76646774a363736ba842b64d4b0
André Gustavo de Andrade <aandrade1234@gmail.com>
2015-07-19 11:44:24 -0300

    Correções para produtos configuráveis

--- app/code/community/PedroTeixeira/Correios/Model/Carrier/CorreiosMethod.php
+++ app/code/community/PedroTeixeira/Correios/Model/Carrier/CorreiosMethod.php
@@ -261,6 +261,7 @@ class PedroTeixeira_Correios_Model_Carrier_CorreiosMethod
                         }
                 }
 	        $this->_packageWeight = $this->_packageWeight + ($prod->getWeight() * $qtd);
+            }
         }
     }
 
