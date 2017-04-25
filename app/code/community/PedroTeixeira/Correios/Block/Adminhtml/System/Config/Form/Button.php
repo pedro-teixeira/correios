a468cbd72cbf0669d032b89210acbf749ca6b3fc
Rafael Patro <rafaelpatro@gmail.com>
2017-04-25 16:41:40 -0300

    #212 Alterações cosméticas

--- app/code/community/PedroTeixeira/Correios/Block/Adminhtml/System/Config/Form/Button.php
+++ app/code/community/PedroTeixeira/Correios/Block/Adminhtml/System/Config/Form/Button.php
@@ -14,20 +14,31 @@
 class PedroTeixeira_Correios_Block_Adminhtml_System_Config_Form_Button
     extends Mage_Adminhtml_Block_System_Config_Form_Field
 {
+    /**
+     * (non-PHPdoc)
+     * @see Mage_Adminhtml_Block_System_Config_Form_Field::_getElementHtml()
+     */
     protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
     {
         return $this->getButton()->toHtml();
     }
     
+    /**
+     * Retrieve an adminhtml button
+     * 
+     * @return string
+     */
     public function getButton()
     {
         $location = Mage::helper('adminhtml')->getUrl('*/sigepweb/postmethodsUpdate');
-        $updateButton = $this->getLayout()->createBlock('adminhtml/widget_button')
-            ->setData(array(
+        $updateButton = $this->getLayout()->createBlock('adminhtml/widget_button');
+        $updateButton->setData(
+            array(
                 'id'      => 'btn-update-postmethods',
                 'label'   => $this->helper('adminhtml')->__('Update'),
                 'onclick' => "setLocation('{$location}')",
-            ));
+            )
+        );
         return $updateButton;
     }
 }
--- app/code/community/PedroTeixeira/Correios/Helper/Data.php
+++ app/code/community/PedroTeixeira/Correios/Helper/Data.php
@@ -13,12 +13,26 @@
  */
 class PedroTeixeira_Correios_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    /**
+     * Gets the configuration value by path
+     * 
+     * @param string $path
+     * 
+     * @return mixed
+     */
     public function getConfigData($path)
     {
         $moduleName = strtolower($this->_getModuleName());
         return Mage::getStoreConfig("carriers/{$moduleName}/{$path}");
     }
     
+    /**
+     * Get a text for option value
+     * 
+     * @param string|int $value
+     * 
+     * @return string|bool
+     */
     public function getShippingLabel($value)
     {
         $source = Mage::getSingleton('pedroteixeira_correios/source_postMethods');
