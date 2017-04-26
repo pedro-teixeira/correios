3c2852794b5fc194b5882e2cdfe9e40110c292bc
Rafael Patro <rafaelpatro@gmail.com>
2017-04-26 16:54:16 -0300

    #217 Alteração cosmética

--- app/code/community/PedroTeixeira/Correios/Model/Postmethod.php
+++ app/code/community/PedroTeixeira/Correios/Model/Postmethod.php
@@ -14,6 +14,9 @@
  * @method string getMethodId()
  * @method string getMethodCode()
  * @method string getMethodTitle()
+ * @method PedroTeixeira_Correios_Model_Postmethod setMethodId(string|int $id)
+ * @method PedroTeixeira_Correios_Model_Postmethod setMethodCode(string $code)
+ * @method PedroTeixeira_Correios_Model_Postmethod setMethodTitle(string $title)
  * @method PedroTeixeira_Correios_Model_Postmethod[] getCollection()
  */
 class PedroTeixeira_Correios_Model_Postmethod extends Mage_Core_Model_Abstract
