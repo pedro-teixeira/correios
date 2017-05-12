876fa437758e75b049bf652f4dbf645657b5107c
Rafael Patro <rafaelpatro@gmail.com>
2017-05-12 10:17:46 -0300

    #251 Adicionada atualização mysql das versões 4.7.2 e 4.7.3

---
+++ app/code/community/PedroTeixeira/Correios/sql/pedroteixeira_correios_setup/upgrade-4.8.0-4.8.1.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This source file is subject to the MIT License.
+ * It is also available through http://opensource.org/licenses/MIT
+ *
+ * @category  PedroTeixeira
+ * @package   PedroTeixeira_Correios
+ * @author    Pedro Teixeira <hello@pedroteixeira.io>
+ * @copyright 2015 Pedro Teixeira (http://pedroteixeira.io)
+ * @license   http://opensource.org/licenses/MIT MIT
+ * @link      https://github.com/pedro-teixeira/correios
+ */
+
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+$installer->startSetup();
+$installer->endSetup();
